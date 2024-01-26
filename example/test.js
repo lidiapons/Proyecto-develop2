fe2cbc740655a3022b502387c19994e5da2dfba8
lidiapons <lidia_garruchera@hotmail.com>
2024-01-26 20:30:40 +0100

    26/01

--- example/test.js
+++ example/test.js
@@ -1,18 +1,97 @@
-let greeting = "Hello";
-let myName = "Lidia";
-const contentElement = document.getElementById("content");
 
-let message = "Result:";
+// function average2(num1, num2){
+//     return (num1 + num2) / 2;
+// }
 
-const num1 = 8;
-const num2 = 14;
 
-const average = (num1 + num2) / 2;
 
-message += ` ${average}`;
+// let greeting = "Hello";
+// let myName = "Lidia";
+// const contentElement = document.getElementById("content");
 
-contentElement.innerHTML =`
-<h1 id="title">
-${message}
-</h1>
-`;
+// // let message = "Result:";
+
+// const num1 = 20;
+// const num2 = 150;
+
+// const average = (num1 + num2) / 2;
+
+// message += ` ${average}`;
+
+// const result = average2(5,7);
+// let messagge =`Result: ${resutl}`;
+
+
+// contentElement.innerHTML =`
+// <p>
+// ${message}
+// </p>
+// `;
+
+// const contentElement = document.getElementById("content");
+// let myName = "Lidia";
+// const message = `Hello ${myName} ,How are you doing today?`;
+// contentElement.innerHTML = message;
+
+// const message = "Hello" + myName + ".How are you doing today?"
+
+// function evenOrOdd(num) {
+//     if (num % 2 == 0) {
+//         return `Number ${num} is even`
+//     } else {
+//         return `Number ${num} is odd`
+//     }
+//  };
+
+// const message = evenOrOdd(4);
+
+// const contentElement = document.getElementById("content");
+// contentElement.innerHTML = message;
+
+
+
+// function removeVowels(text){
+//     const vowels ="aeiou";
+//     const length = text.lenght;
+//     let result = "";
+
+//     for (let i=0;index < length; index++){
+//         if (!vowels.includes(text.charAt(i))) {
+//             result += text.charAt(i);
+//         }
+// }
+// return result;
+
+// }
+
+// const contentElement = document.getElementById("content");
+// contentElement.innerHTML = removeVowels("hola mundo");
+
+
+/*
+crear funcion
+declarar const length
+declarar variable result inicializada con un string vacío
+recorrer cada uno de los caracteres usando un for
+declarar let "char" con el caracter en cada posicion
+usando otro bucle for, repetimos "char" ese numero de veces
+usar if-else para saber si es la primera repeticion
+si es una primera, en mayusculas
+
+
+*/
+
+
+
+function repetitions(text){
+    const lenght = text.lenght;
+    let result = "";
+    for (let i=0,i < length,i++){
+        for ( ){
+            if
+        }
+    }
+}
+
+
+
