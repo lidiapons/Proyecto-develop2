c07389f0dd9aa6e46c759ddc9fd845d106be71e4
lidiapons <lidia_garruchera@hotmail.com>
2024-01-30 20:32:19 +0100

    30/01

--- example/test.js
+++ example/test.js
@@ -83,15 +83,69 @@ si es una primera, en mayusculas
 
 
 
-function repetitions(text){
-    const lenght = text.lenght;
-    let result = "";
-    for (let i=0,i < length,i++){
-        for ( ){
-            if
-        }
-    }
+// function repetitions(text){
+//     const lenght = text.lenght;
+//     let result = "";
+//     for (let i=0,i < length,i++){
+//         for ( ){
+//             if
+//         }
+//     }
+// }
+
+
+// function isPalindrome(text){
+//     if (typeof text === "string"){
+
+//         const accents = "áéíóú";
+//         const vowels = "aeiou";
+
+//         let formattedText = text.toLowerCase().replaceAll(" ","");
+
+//     for (let i=0;i < accents.length;i++){
+//         formattedText = formattedText.replaceAll(accents.charAt(i), vowels.charAt(i));
+//     }
+
+//     for (let i=0;i < formattedText.length;i++){
+//         const firstIndex = i;
+//         const lastIndex = formattedText.length -i -1;
+
+//         if (firstIndex >= lastIndex){
+//             return true;
+//         }
+
+//         if (formattedText.charAt(firstIndex) !== formattedText.charAt(lastIndex)){
+//             return false;
+//             }
+//     }
+//         return true;
+//     }
+//     return false;
+// }
+
+
+// console.log(isPalindrome("Dábale arroz a la zorra el abad"));
+function show(htmlcontent){
+    document.getElementById("content").innerHTML += htmlcontent;
 }
 
+function suma(op1, op2){
+    return op1 + op2;
+}
+function resta(op1, op2){
+    return op1 - op2;
+}
+function multiplicar(op1, op2){
+    return op1 * op2;
+}
+function dividir(op1, op2){
+    return op1 / op2;
+}
 
+function calculator(op1, op2, opnombre, fn){
+    return `<br>El resultado de ${opnombre} ${op1} y ${op2} es ${fn(op1, op2)}</br>`
+}
 
+show(calculator(2,3, "suma", suma));
+show(calculator(5,3, "restar", resta));
+show(calculator(8,3, "multiplicar", multiplicar));
